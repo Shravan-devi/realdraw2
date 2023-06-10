f78d2d8fa3914699c479337bf66eaa9a1a5f8490
Shravan-devi <83762735+Shravan-devi@users.noreply.github.com>
2023-06-10 17:16:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-
+noseX=0;
+noseY=0;
+difference=0;
+leftwristX=0;
+rightwristX=0;
 
   function setup() {
   video = createCapture(VIDEO);
@@ -21,10 +25,18 @@ function gotPoses(results)
   if(results.length > 0)
   {
     console.log(results);
-    
+    leftwristX= results[0].pose.leftWrist.x;
+    rightwristX= results[0].pose.rightWrist.x;
+    difference= floor(leftwristX-rightwristX);
+    noseX= results[0].pose.nose.x;
+    noseY= results[0].pose.nose.y;    
   }
 }
 
 function draw() {
 background('#969A97');
+fill("#6262d1");
+stroke("#6262d1");
+rectMode(CENTER);
+square(noseX, noseY, difference);
 }
